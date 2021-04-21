31c4240792bc982e06fcfe7852d5ae6a35ed8a35
cayke <thecactivecake@gmail.com>
2021-04-21 14:23:29 +1000

    TACO

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,23 +1,5 @@
 use takh_client::{settings::Settings, window::Window, GlobalState};
 
 fn main() {
-	// Establish our logger
-	simple_logger::SimpleLogger::new()
-		.with_level(log::LevelFilter::Warn)
-		.init()
-		.ok();
-
-	// Load settings
-	let settings = Settings::load();
-	// Create our window and opengl context
-	let (window, event_loop) = match Window::new(&settings) {
-		Ok(o) => o,
-		Err(e) => {
-			log::error!("failed to create window: {:?}", e);
-			std::process::exit(1);
-		}
-	};
-	// Create our global state struct and run the event loop
-	let global_state = GlobalState { settings, window };
-	takh_client::run(global_state, event_loop);
+	println!("asdasd");
 }
